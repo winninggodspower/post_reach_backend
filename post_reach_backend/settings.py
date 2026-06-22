a15e6eed6285530017cd2eb8932f8a365c557e85
winning godspower <winninggodspower@gmail.com>
2026-06-22 11:50:41 +0100

    fix linting check

--- post_reach_backend/settings.py
+++ post_reach_backend/settings.py
@@ -1,7 +1,8 @@
+import os
 from datetime import timedelta
 from pathlib import Path
+
 import environ
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
